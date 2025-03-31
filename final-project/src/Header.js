fc01cd1b3496527227c857e0aca7d4279abeb44c
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-03-31 16:25:56 -0600

    Searchbar placement done could be loower but mvp is ready

--- final-project/src/Header.js
+++ final-project/src/Header.js
@@ -6,31 +6,58 @@ import styled from "styled-components";
 
 
 const StyledHeader = styled.header`
-background-color : #393939;
-box-shadow : 0 3px 3px rgba(0,0,0,.2)  
-display: grid;
-grid-template-columns: 200px 4fr 200px;
+  background-color: #393939;
+  box-shadow: 0 3px 3px rgba(0,0,0,.2);
+  display:grid;
+  grid-template-columns: 200px 1fr 200px;
+  grid-column-gap: 20px;
 `;
+
 
 const LogoLink = styled.a`
 color: #fff;
 text-decoration: none;
 display : inline-block;
 height: 50px;
-line-height: 50px;
-padding: 0 15px;
+line-height: 30px;
+padding: 0px 15px;
+svg{
+margin-top: 10px;
+display: inline-block;
+float: left;
+}
+span{
+    display: inline-block;
+    padding-left: 5px;
+    padding-top: 15px
+
+}
 b{
     font-weight: bold;    
 }
 `;
 
+const SearchInput = styled.input`
+    display: inline-block;
+    box-sizing: border-box;
+    width: 100%;
+    border-raidus: 3px;
+    border: 1px solid #777;
+    background: rgba(0,0,0,.1);
+    padding: 8px 10px;
+    margin top: 9 px;
+`;
+
 function Header(){
     return(<StyledHeader>
         <LogoLink href ="" className="logo">
-        <FontAwesomeIcon icon= {faClipboardQuestion} />
-            <b>Q and A</b></LogoLink>
+        <FontAwesomeIcon icon= {faClipboardQuestion}  size ="2x"/>
+            <span>
+            <b>Q and A</b>
+            </span>
+            </LogoLink>
         <form action="" className="search">
-          <input type="text" placeholder="Search..."/>
+          <SearchInput type="text" placeholder="Search..."/>
         </form>
         <a href = "" className="profile">Melwin</a>
      </StyledHeader>
