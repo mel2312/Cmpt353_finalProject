995c0870e625b54d8fafb044b6d200d6abe9b55d
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-03-31 11:47:55 -0600

    header icon is working not able to put search and profile in right place

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -3,9 +3,11 @@ import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
   body {
     background: #2d2d2d;
     color: #fff;
+    font-family: Roboto, sans-serif;
   }
 `;
 function App() {
--- final-project/src/Header.js
+++ final-project/src/Header.js
@@ -3,10 +3,26 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
 background-color : #393939;
+box-shadow : 0 3px 3px rgba(0,0,0,.2)  
+display: grid;
+grid-template-columns: 200px 4fr 200px;
 `;
+
+const LogoLink = styled.a`
+color: #fff;
+text-decoration: none;
+display : inline-block;
+height: 50px;
+line-height: 50px;
+padding: 0 15px;
+b{
+    font-weight: bold;    
+}
+`;
+
 function Header(){
     return(<StyledHeader>
-        <a href ="" className="logo">Q and A</a>
+        <LogoLink href ="" className="logo"><b>Q and A</b></LogoLink>
         <form action="" className="search">
           <input type="text" placeholder="Search..."/>
         </form>
