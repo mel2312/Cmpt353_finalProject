8635ec7f930524bf727c97c48f24321bd2846fab
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-03-31 10:59:59 -0600

    able to give black background

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,8 +1,17 @@
+import { Reset } from 'styled-reset';
+import styled, { createGlobalStyle } from 'styled-components';
 
-
+const GlobalStyles = createGlobalStyle`
+  body {
+    background: #2d2d2d;
+    color: #fff;
+  }
+`;
 function App() {
   return (
     <div>
+      <Reset />
+      <GlobalStyles/>
       <header>
         <a href ="" className="logo">Q and A</a>
         <form action="" className="search">
