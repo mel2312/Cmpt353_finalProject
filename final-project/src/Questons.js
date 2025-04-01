9549621f8d4c473def74afca193f33b1a37ed125
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-04-01 15:16:16 -0600

    votes,views and answers to the MVP

--- final-project/src/Questons.js
+++ final-project/src/Questons.js
@@ -17,8 +17,41 @@ const BlueButton = styled.button`
     color: #fff;
     border:0;
     border-raidus: 5px;
-    padding: 15px 10px;
+    padding: 12px 10px;
 
+`;
+
+const QuestionRow = styled.div`
+    background-color: rgba(225,225,225,.1)
+    padding: 15px 15px;
+    display: grid;
+    grid-template-columns: repeat(3,50px) 1fr; 
+
+
+`;
+
+const QuestionStat= styled.div`
+    text-align: center;
+    display: inline-block;
+    font-size: 1.2rem;
+    span{
+        font-size: .7rem;
+        display:block;
+        font-weight: 300;
+        margin-top: 4px;
+
+        }
+`;
+
+const QuestionTitleArea = styled.div`
+    padding: 0 30px;
+`;
+
+const Tag = styled.span`
+    display: inline-block;
+    margin-right: 3px;
+    background-color: #3e4a52
+    color: #9cc3db
 `;
 
 function Question(){
@@ -28,7 +61,12 @@ function Question(){
                 <StyledHeader>Questions</StyledHeader>
                 <BlueButton>Ask&nbsp;Question</BlueButton>
             </HeaderRow>         
-                
+            <QuestionRow>
+                <QuestionStat>0 <span>votes</span></QuestionStat>
+                <QuestionStat>0 <span>answers</span></QuestionStat>
+                <QuestionStat>0 <span>views</span></QuestionStat>
+                <QuestionTitleArea>Message of the channels</QuestionTitleArea>
+            </QuestionRow>
             
         </main>
     
