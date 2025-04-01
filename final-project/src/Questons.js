4854f823ce0bc29e10c943ab402c619b0481376b
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-04-01 12:35:18 -0600

    Ask Question button modified to MVP

--- final-project/src/Questons.js
+++ final-project/src/Questons.js
@@ -12,12 +12,21 @@ const HeaderRow = styled.div`
 
 `;
 
+const BlueButton = styled.button`
+    background-color: #378ad3;
+    color: #fff;
+    border:0;
+    border-raidus: 5px;
+    padding: 15px 10px;
+
+`;
+
 function Question(){
     return(
         <main>
             <HeaderRow>
                 <StyledHeader>Questions</StyledHeader>
-                <button>Ask&nbsp;Question</button>
+                <BlueButton>Ask&nbsp;Question</BlueButton>
             </HeaderRow>         
                 
             
