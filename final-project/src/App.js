72f43b8240f812ae0377a5e76413ed22d37752e1
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-04-02 17:30:38 -0600

    Intial setting up of routing

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,6 +2,14 @@ import { Reset } from 'styled-reset';
 import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 import Question from './Questions';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import AskPage from './AskPage'
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Roboto:ital,wght@0,300..900;1,100..900&display=swap');
@@ -17,7 +25,13 @@ function App() {
       <Reset />
       <GlobalStyles/>
       <Header/>
-      <Question/>
+      
+      <Router>
+        <Routes>
+          <Route path='/ask' Component={AskPage}/>
+          <Route path='/' Component={Question} />
+        </Routes>
+      </Router>
     </div>
       
   );
