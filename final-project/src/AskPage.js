f3f44c254e76efd9293e3dc37c14beb4157db16b
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-04-02 18:45:37 -0600

    No button for uploading question just feild for tittle of the question and description but no screenshot field

--- final-project/src/AskPage.js
+++ final-project/src/AskPage.js
@@ -1,5 +1,35 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    padding:30px 20px;
+`;
+
+const QuestionTittleInput = styled.input`
+    background: none;
+    border: 1px solid #777;
+    border-radius: 3px;
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10 px;
+    margin-bottom: 20px;
+`;
+
+const QuestionBodyTextArea= styled.textarea`
+    background: none;
+    border: 1px solid #777;
+    border-radius: 3px;
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10 px;
+    min-height: 200px;
+`;
 export default function AskPage(){
     return(
-        <div>Asks a Question</div>
+        <Container>
+            <QuestionTittleInput type="text" placeholder= "Title of your question"/>
+            <QuestionBodyTextArea placeholder="Type your description of your question here"/>
+        </Container>
     )
 }
