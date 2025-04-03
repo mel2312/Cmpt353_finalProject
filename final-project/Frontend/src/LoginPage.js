9f8cac5445b8c829060f0100e5c60530e0c667d9
Melwin P Sebastian <melwinperumpral@gmail.com>
2025-04-03 16:09:11 -0600

    Setup for backend to be implemented

--- final-project/Frontend/src/LoginPage.js
+++ final-project/Frontend/src/LoginPage.js
@@ -1,22 +1,44 @@
+import { Component } from "react";
 import Header1 from "./Header1"
 import styled from "styled-components";
 import StyledInput from "./StyledInput";
 import BlueButton from "./BlueButton";
+import axios from 'axios';
 
 const Container = styled.div`
     padding: 30px 20px;
 `;
 
-function LoginPage(){
+class LoginPage extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+    login(){
+        axios.post('http://localhost:3030/login',{
+            email: this.state.email,
+            password: this.state.password,
+
+        })
+    }
+    render(){
     return(<>
     <Container>
         <Header1 style={{margin:0}}>Login</Header1>
-        <StyledInput placeholder="email" type="email"/>
-        <StyledInput placeholder="password" type= "password"/>
-        <BlueButton>Login</BlueButton>
+        <StyledInput placeholder="email" type="email" value={this.state.email}
+            onChange={ev => this.setState({email:ev.target.value})}
+        />
+        <StyledInput placeholder="password" type= "password" value={this.state.password}
+            onChange={ev => this.setState({password:ev.target.value})}  
+        />
+        <BlueButton onClick={() => this.login()}>Login</BlueButton>
     </Container>
         
     </>);
+    }
 }
 
 export default LoginPage
